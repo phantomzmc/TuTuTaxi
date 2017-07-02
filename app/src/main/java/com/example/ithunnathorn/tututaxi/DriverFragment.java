d50b8fd910357a7de291d2fcda03bd743ef2fd90
Thunnathorn <phantomzmc@gmail.com>
2017-07-02 14:21:48 +0700

    Revert "Revert "realtime database and auth email with firebase""
    This reverts commit 244e4a9c6d26b2fd5fdf1fe18d43e0b453922e5b.

--- app/src/main/java/com/example/ithunnathorn/tututaxi/DriverFragment.java
+++ app/src/main/java/com/example/ithunnathorn/tututaxi/DriverFragment.java
@@ -3,10 +3,31 @@ package com.example.ithunnathorn.tututaxi;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.concurrent.Executor;
+
+import static android.content.ContentValues.TAG;
 
 
 /**
@@ -26,6 +47,17 @@ public class DriverFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    View view;
+    private FirebaseAuth mAuth;
+    private DatabaseReference myRef;
+    private FirebaseDatabase database;
+
+    EditText emailEditText;
+    EditText passwordEditText;
+    Button singinButton,buttonWrite,buttonRead;
+    Button singupButton;
+    TextView resouceTextView;
+
 
     private OnFragmentInteractionListener mListener;
 
@@ -58,14 +90,134 @@ public class DriverFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        mAuth = FirebaseAuth.getInstance();
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String resouceTextView = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + resouceTextView);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_driver, container, false);
+        view = inflater.inflate(R.layout.fragment_driver, container, false);
+        emailEditText =(EditText) view.findViewById(R.id.emailEditText);
+        passwordEditText = (EditText) view.findViewById(R.id.passwordEditText);
+        singinButton = (Button) view.findViewById(R.id.buttonSignIn);
+        singupButton = (Button) view.findViewById(R.id.buttonSignUp);
+        buttonWrite = (Button) view.findViewById(R.id.buttonWrite);
+        buttonRead = (Button) view.findViewById(R.id.buttonRead);
+        resouceTextView = (TextView) view.findViewById(R.id.resoureTextView);
+
+
+        singupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp(emailEditText.getText().toString(),passwordEditText.getText().toString());
+            }
+        });
+        singinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn(emailEditText.getText().toString(),passwordEditText.getText().toString());
+            }
+        });
+        buttonWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                realTime(emailEditText.getText().toString());
+                Toast.makeText(DriverFragment.this.getContext(),
+                        "Sent Messager to Firebase", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
     }
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null){
+
+        }
+
+    }
+//
+
+    public void signUp(String email,String password){
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener((Executor)DriverFragment.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if (user != null){
+
+                            }
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(DriverFragment.this.getContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+    }
+
+    public void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if (user != null){
+
+                            }
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(DriverFragment.this.getContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void realTime(String text) {
+        myRef.setValue(text);
+    }
+
+
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
